8917642fd4465faf3e2a6886b48018226235b548
gz729211705 <729211705@qq.com>
2017-01-02 10:47:07 +0800

    The function of continue in loops

--- break_and_continue.cpp
+++ break_and_continue.cpp
@@ -4,11 +4,18 @@ int main()
 {
 	int n,sum = 0;
 	
-	for(int i = 1; i<=1000;i++)
+	/*for(int i = 1; i<=1000;i++)
 	{
 		sum += i;
 		if(sum >= 2000)
 			break;
+	}*/
+	
+	for(int i = 1; i<=5; i++)
+	{
+		if(i%2 == 0)
+			continue;
+		sum += i;
 	}
 	
 	printf("sum = %d\n", sum);
